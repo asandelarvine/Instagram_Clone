e706464709d40dfaff34fce71cb2cd2a7037af82
asandelarvine <asandelarvine@gmail.com>
2021-12-06 15:10:28 +0300

    registerd image and follows

--- insta/admin.py
+++ insta/admin.py
@@ -4,5 +4,5 @@ from .models import Profile,Like,Comment,Image,Follows
 admin.site.register(Profile)
 admin.site.register(Like)
 admin.site.register(Comment)
-# admin.site.register(Image)
-# admin.site.register(Follows)
+admin.site.register(Image)
+admin.site.register(Follows)
