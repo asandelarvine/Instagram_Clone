d361eab545d1fd252fbbfdf78bf0ebdab1475bea
asandelarvine <asandelarvine@gmail.com>
2021-12-06 16:17:24 +0300

    imported Image

--- insta/forms.py
+++ insta/forms.py
@@ -9,7 +9,7 @@ class Registration(UserCreationForm):
 
   class Meta:
     model = User
-    fields = ['username','email','password1','password2']
+    fields = ['username','email','password1']
 
 
 class UpdateUser(forms.ModelForm):
