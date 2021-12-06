9af84870fc464a624d528122eaa9575e26067b79
asandelarvine <asandelarvine@gmail.com>
2021-12-06 14:58:50 +0300

    imported Like,Comment

--- insta/admin.py
+++ insta/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Profile,Like,Comment,Image,Follows
 
 admin.site.register(Profile)
-# admin.site.register(Like)
-# admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Comment)
 # admin.site.register(Image)
 # admin.site.register(Follows)
