7583b0b30eb40d88ca6b217655f7424bc375b16e
asandelarvine <asandelarvine@gmail.com>
2021-12-06 16:19:23 +0300

    added a second password

--- insta/forms.py
+++ insta/forms.py
@@ -9,7 +9,7 @@ class Registration(UserCreationForm):
 
   class Meta:
     model = User
-    fields = ['username','email','password1']
+    fields = ['username','email','password1','password2']
 
 
 class UpdateUser(forms.ModelForm):
