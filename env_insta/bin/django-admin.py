100e7cda15b9200fb6d9756b5f2b800cdcbdebce
asandelarvine <asandelarvine@gmail.com>
2021-12-06 10:09:08 +0300

    first commit

---
+++ env_insta/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/access/Documents/Django/Django_Projects/Eens~tah~grahm/env_insta/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
