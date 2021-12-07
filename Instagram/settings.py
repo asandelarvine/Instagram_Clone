7590eae64033b667260992de76eff6eddf3b1201
asandelarvine <asandelarvine@gmail.com>
2021-12-07 16:40:36 +0300

    adding a base path directory for the project

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -30,7 +30,7 @@ cloudinary.config(
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MODE = config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
